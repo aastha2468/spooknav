b71dfe906aeadda2ae2968944a71e753a98a2b7b
aastha2468 <68781410+aastha2468@users.noreply.github.com>
2020-10-25 00:47:10 -0400

    Update draftJS.js

--- draftJS.js
+++ draftJS.js
@@ -1,39 +1,48 @@
 // TO MAKE THE MAP APPEAR YOU MUST
 // ADD YOUR ACCESS TOKEN FROM
 // https://account.mapbox.com
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWFzdGhhMjQ2OCIsImEiOiJja2dvMmF2c2cwbnR1MnRwdHc1Y2tiZDhyIn0.JnBCJ-bz22pFEdGpUq1Vuw';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWFzdGhhMjQ2OCIsImEiOiJja2dvMmF2c2cwbnR1MnRwdHc1Y2tiZDhyIn0.JnBCJ-bz22pFEdGpUq1Vuw";
 
 var map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  container: "map", // container id
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: [-76.6122, 39.2904], // starting position [lng, lat]
+  zoom: 15 // starting zoom
 });
 
 // add layers
-var layerList = document.getElementById('menu');
-var inputs = layerList.getElementsByTagName('input');
+var layerList = document.getElementById("menu");
+var inputs = layerList.getElementsByTagName("input");
 
 function switchLayer(layer) {
-var layerId = layer.target.id;
-map.setStyle('mapbox://styles/mapbox/' + layerId);
-}
- 
-for (var i = 0; i < inputs.length; i++) {
-inputs[i].onclick = switchLayer;
+  var layerId = layer.target.id;
+  map.setStyle("mapbox://styles/mapbox/" + layerId);
 }
 
-// create the popup
-var popup = new mapboxgl.Popup({ offset: 25 }).setText(
-'Here is more data'
-);
- 
-// create DOM element for the marker
-var el = document.createElement('div');
-el.id = 'marker';
- 
-// create the marker
-var marker = new mapboxgl.Marker(el)
-.setLngLat([-74.5, 40])
-.setPopup(popup) // sets a popup on this marker
-.addTo(map);
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].onclick = switchLayer;
+}
+
+d3.csv(
+  "https://raw.githubusercontent.com/aastha2468/spooknav/main/finalDataTester.csv"
+).then(function (data) {
+  for (var i = 0; i < data.length; i++) {
+    var popup = new mapboxgl.Popup({ offset: 25 }).setText(data[i].String);
+    // create DOM element for the marker
+    var d = parseInt(data[i].danger);
+    var el = document.createElement("div");
+    if (d <= 4) {
+      el.id = "marker1";
+    } else if (d <= 6) {
+      el.id = "marker2";
+    } else {
+      el.id = "marker3";
+    }
+    new mapboxgl.Marker(el)
+      .setLngLat([data[i].longitude, data[i].latitude])
+      .setPopup(popup) // sets a popup on this marker
+      .addTo(map);
+  }
+  console.log(data[3]); // [{"Hello": "world"}, …]
+});
